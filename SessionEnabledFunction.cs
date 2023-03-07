367a1e2e335a050ef65aad6b5529ae0802d8a78b
Saurabh Agarwal <saurabh.agarwal@ahanyatech.com>
2023-03-07 13:20:13 -0600

    Baseline Code

---
+++ SessionEnabledFunction.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace SessionEnabledFunction
+{
+    public class SessionEnabledFunction
+    {
+        [FunctionName("SessionEnabledFunction")]
+        public void Run([ServiceBusTrigger("%QueueName%", Connection = "ServiceBusConString", IsSessionsEnabled = true)]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"{myQueueItem}");
+        }
+    }
+}
